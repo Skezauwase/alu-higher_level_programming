49ac6e4d7f1c0ee38c7035e6e80ae86b40de4a46
keza <s.uwase4@alustudent.com>
2025-11-28 20:03:25 +0200

    changing

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Module 0-hbtn_status.
-Fetches https://intranet.hbtn.io/status using urllib.
-"""
+"""I document you"""
+
+
 import urllib.request
 
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body)))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
