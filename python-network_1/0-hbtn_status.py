5ca3385ca48b33635a8bc105dedd455d435964f7
keza <s.uwase4@alustudent.com>
2025-11-28 18:28:32 +0200

    new commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
-
 #!/usr/bin/python3
 """Module 0-hbtn_status.
 Fetches https://intranet.hbtn.io/status using urllib.
 """
-import urllib.requestwith 
+import urllib.request
 
-urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
     body = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+    print("\t- content: {}".format(body)))
     print("\t- utf8 content: {}".format(body.decode("utf-8")))
