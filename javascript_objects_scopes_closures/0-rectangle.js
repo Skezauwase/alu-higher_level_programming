ad64d3f8777e7b3168f5d6de5f23e190c0876a4a
keza <s.uwase4@alustudent.com>
2026-01-30 13:25:32 +0200

    java objects

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, contents) => {
-  err ? console.log(err) : console.log(contents.toString());
-});
+// define an empty class Rectangle
+class Rectangle {}
+
+module.exports = Rectangle;
